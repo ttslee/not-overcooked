53ec7d66a24e3637afa51cefbedbc221734065b8
ttslee <theodol2@uci.edu>
2020-01-13 14:29:35 -0800

    added interactable and removed player2 controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,13 +9,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private Rigidbody2D playerBody;
 
+    public int player = 1;
 
+    private string horizontal, vertical;
     Vector2 movement;
-
+    private void Start()
+    {
+        horizontal = (player == 1) ? "Horizontal" : "Horizontal2";
+        vertical = (player == 1) ? "Vertical" : "Vertical2";
+    }
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw(horizontal);
+        movement.y = Input.GetAxisRaw(vertical);
         if (movement.x == -1)
         {
             transform.localRotation = Quaternion.Euler(0, 180, 0);
