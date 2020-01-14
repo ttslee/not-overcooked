ce1c02872173ec8e986bb6edaf820af3750a4d4d
Johnny Ngo <jdngo2@uci.edu>
2020-01-14 15:00:22 -0800

    Added Unfinished Camera

---
+++ Assets/Scripts/MultiTargetCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiTargetCamera : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> players;
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private float smoothTime = .5f;
+
+    private Vector3 velocity;
+
+
+    void LateUpdate()
+    {
+        if (players.Count == 0) return;
+        Vector3 centerPoint = GetCenterPoint();
+        Vector3 newPosition = centerPoint + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (players.Count == 1)
+        {
+            return players[0].position;
+        }
+
+        var bounds = new Bounds(players[0].position, Vector3.zero);
+        for (int i = 0; i < players.Count; i++)
+        {
+            bounds.Encapsulate(players[i].position);
+        }
+
+        return bounds.center;
+    }
+}
