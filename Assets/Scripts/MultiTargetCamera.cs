974a66bb0d942e5fd6fed32fecb1727ad198defc
Johnny Ngo <jdngo2@uci.edu>
2020-01-14 18:08:30 -0800

    Finished MultiTargetCamera

--- Assets/Scripts/MultiTargetCamera.cs
+++ Assets/Scripts/MultiTargetCamera.cs
@@ -2,39 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class MultiTargetCamera : MonoBehaviour
 {
-    [SerializeField]
-    private List<Transform> players;
+    public List<Transform> players;
     [SerializeField]
     private Vector3 offset;
     [SerializeField]
     private float smoothTime = .5f;
+    public float minSize = 4f;
+    public float maxSize = 7f;
+    public float zoomDelay = 5f;
 
     private Vector3 velocity;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
 
 
     void LateUpdate()
     {
         if (players.Count == 0) return;
+        Move();
+        Zoom();
+    }
+
+    void Move()
+    {
         Vector3 centerPoint = GetCenterPoint();
         Vector3 newPosition = centerPoint + offset;
         transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
     }
 
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(minSize, maxSize, GetGreatestDistance() / zoomDelay);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, newZoom, Time.deltaTime);
+    }
+
     Vector3 GetCenterPoint()
     {
-        if (players.Count == 1)
-        {
-            return players[0].position;
-        }
+        return GetEncapsulatingBounds().center;
+    }
 
+    float GetGreatestDistance()
+    {
+        return GetEncapsulatingBounds().size.x;
+    }
+
+    Bounds GetEncapsulatingBounds()
+    {
         var bounds = new Bounds(players[0].position, Vector3.zero);
         for (int i = 0; i < players.Count; i++)
         {
             bounds.Encapsulate(players[i].position);
         }
 
-        return bounds.center;
+        return bounds;
     }
 }
