183c26d7dc9a1198566b3063f50e272a388e5642
ttslee <theodol2@uci.edu>
2020-01-15 18:19:39 -0800

    merge

--- Assets/Scripts/MonsterManager.cs
+++ Assets/Scripts/MonsterManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MonsterManager : MonoBehaviour
 {
+    public struct Recipe
+    {
+
+    }
     // Start is called before the first frame update
     void Start()
     {
