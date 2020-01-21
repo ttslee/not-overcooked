8fb626130330719909110b9a38d869c059b95acc
Johnny Ngo <jdngo2@uci.edu>
2020-01-21 00:16:37 -0800

    simple enemyai!!!!!!!!

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public List<Transform> players;
+    public float moveSpd = 1f;
+    public float stoppingDistance;
+
+
+    void Update()
+    {
+        if (players.Count == 0) return;
+        Move();
+    }
+
+    void Move()
+    {
+        Transform target = GetClosestTarget(players[0], players[1]);
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpd * Time.deltaTime);
+        } //else shoot projectile or something
+    }
+
+    Transform GetClosestTarget(Transform object1, Transform object2)
+    {
+        float target1 = GetDistance(gameObject.transform, object1);
+        float target2 = GetDistance(gameObject.transform, object2);
+        if (target1 <= target2) return object1;
+        else return object2;
+    }
+
+    float GetDistance(Transform object1, Transform object2)
+    {
+        return Vector3.Distance(object1.transform.position, object2.transform.position); ;
+    }
+
+
+}
