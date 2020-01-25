ccf186017fea43b2d72b2e905e719a0c5268791c
Johnny Ngo <jdngo2@uci.edu>
2020-01-24 21:05:44 -0800

    rat wandering ai movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -9,25 +9,56 @@ public class EnemyMovement : MonoBehaviour
     public float moveSpd = 1f;
     public float stoppingDistance;
     public bool isWandering = false;
+    public Animator animator;
+    public Transform moveSpot;
+    private float wait;
+    public float waitTime = 2f;
+    public float minX = -9.5f;
+    public float maxX = 9.5f;
+    public float minY = -12.5f;
+    public float maxY = 6.5f;
 
+
+    private void Start()
+    {
+        wait = waitTime;
+        moveSpot.position = CreateMoveSpot();
+    }
 
     void Update()
     {
-        if (players.Count == 0) return;
         if (isWandering)
         {
             Wander();
-        }
+        } else if (players.Count == 0) return;
         else Move();
     }
 
     void Wander()
     {
-        Transform target = GetClosestTarget(players[0], players[1]);
-        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        if (transform.position.x > moveSpot.position.x)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpd * Time.deltaTime);
-        } //else shoot projectile or something
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (transform.position.x < moveSpot.position.x)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, moveSpd * Time.deltaTime);
+        if (Vector2.Distance(transform.position, moveSpot.position) < 0.2f)
+        {
+            if (wait <= 0)
+            {
+                moveSpot.position = CreateMoveSpot();
+                wait = waitTime;
+                animator.SetFloat("Speed", 1f);
+            } else
+            {
+                wait -= Time.deltaTime;
+                animator.SetFloat("Speed", 0f);
+            }
+        }
+
     }
 
     void Move()
@@ -49,7 +80,12 @@ public class EnemyMovement : MonoBehaviour
 
     float GetDistance(Transform object1, Transform object2)
     {
-        return Vector3.Distance(object1.transform.position, object2.transform.position); ;
+        return Vector2.Distance(object1.transform.position, object2.transform.position); 
+    }
+
+    Vector2 CreateMoveSpot()
+    {
+        return new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
     }
 
 
